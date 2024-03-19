37e9291074a0947ad2d513c94b76a7a8d295c3f9
pgmillow <1310336507@qq.com>
2024-03-20 00:52:14 +0800

    第一次作业

---
+++ assignment1/Form02.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num1 = double.Parse(num1TextBox.Text);
+                double num2 = double.Parse(num2TextBox.Text);
+                string selectedOperator = operatorComboBox.SelectedItem.ToString();
+
+                double result = Calculate(num1, num2, selectedOperator);
+
+                resultLabel.Text = "结果: " + result.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("请输入有效的数字！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("发生未知错误：" + ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private double Calculate(double num1, double num2, string op)
+        {
+            switch (op)
+            {
+                case "+":
+                    return num1 + num2;
+                case "-":
+                    return num1 - num2;
+                case "*":
+                    return num1 * num2;
+                case "/":
+                    if (num2 != 0)
+                        return num1 / num2;
+                    else
+                        throw new ArgumentException("除数不能为零");
+                default:
+                    throw new ArgumentException("无效的运算符");
+            }
+        }
+    }
+}
---
+++ assignment1/Program01.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("欢迎使用简单计算器！");
+            Console.WriteLine("请输入第一个数字: ");
+            double num1 = GetNumberFromUser();
+
+            Console.WriteLine("请输入第二个数字: ");
+            double num2 = GetNumberFromUser();
+
+            Console.WriteLine("请输入运算符 (+, -, *, /): ");
+            char op = GetOperatorFromUser();
+
+            try
+            {
+                double result = Calculate(num1, num2, op);
+                Console.WriteLine("计算结果: " + result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("错误: " + ex.Message);
+            }
+
+            Console.ReadLine(); // 保持控制台窗口打开
+        }
+
+        static double GetNumberFromUser()
+        {
+            double num;
+            while (!double.TryParse(Console.ReadLine(), out num))
+            {
+                Console.WriteLine("错误: 请输入有效的数字");
+                Console.WriteLine("请重新输入: ");
+            }
+            return num;
+        }
+
+        static char GetOperatorFromUser()
+        {
+            char op;
+            while (!char.TryParse(Console.ReadLine(), out op) || (op != '+' && op != '-' && op != '*' && op != '/'))
+            {
+                Console.WriteLine("错误: 请输入有效的运算符 (+, -, *, /)");
+                Console.WriteLine("请重新输入: ");
+            }
+            return op;
+        }
+
+        static double Calculate(double num1, double num2, char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                case '*':
+                    return num1 * num2;
+                case '/':
+                    if (num2 != 0)
+                        return num1 / num2;
+                    else
+                        throw new DivideByZeroException("除数不能为零");
+                default:
+                    throw new ArgumentException("无效的运算符");
+            }
+        }
+    }
+}
